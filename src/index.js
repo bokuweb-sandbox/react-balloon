d56f7f197f35125645ba5cf52cd3fe62aef1592b
bokuweb <bokuweb12@gmail.com>
2016-02-13 16:35:12 +0900

    Fix style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Draggable from 'react-draggable';
 
 export default class HelloWorld extends Component {
   static propTypes = {
-    start: PropTypes.object.isRequired
+    start: PropTypes.object.isRequired,
   };
 
   static defaultProps = {
@@ -12,48 +12,70 @@ export default class HelloWorld extends Component {
       x: 0,
       y: 0,
       width: 100,
-      height: 100
-    }
+      height: 100,
+    },
   };
 
   constructor(props) {
     super(props);
     this.state = {
       pointer: {
-        x: 300,
-        y: 300
-      }
+        destination: { x: 300, y: 300 },
+        base: [
+          { x: 100, y: 100 },
+          { x: 100, y: 150 },
+        ],
+      },
     };
   }
 
   onDrag(e) {
+    const { base } = this.state.pointer;
     this.setState({
       pointer: {
-        x: e.clientX,
-        y: e.clientY
-      }
+        base,
+        destination: {
+          x: e.clientX,
+          y: e.clientY,
+        },
+      },
     });
   }
 
   render() {
     const { start } = this.props;
-    const { pointer } = this.state;
+    const { base, destination } = this.state.pointer;
     return (
       <div>
         <Resizable
-           start={ start }
-           customStyle={{background:"#333"}}>
+          start={ start }
+          customStyle={
+             { background: '#333' }
+           }
+        >
           Hello, world
         </Resizable>
-        <svg width="2000" height="2000" onClick={()=>console.log('aa')}>
-          <path d={`M 100 100 Q 100 125 ${ pointer.x } ${ pointer.y } Q 100 125 100 150`} fill="purple"/>
+        <svg width="2000" height="2000">
+          <path
+            d={ `M ${ base[0].x } ${ base[0].y }
+                 Q 100 125 ${ destination.x } ${ destination.y }
+                 Q 100 125 ${ base[1].x } ${ base[1].y }` }
+            fill="purple"
+          />
         </svg>
         <Draggable
-           start={{x:300, y: 300}}
-           onStart={()=>{}}
-           onDrag={::this.onDrag}
-           onStop={()=>{}}>
-          <div style={{width:'20px', height:'20px', background:'#333', position: 'absolute', top: 0, left: 0}}/>
+          start={{ x: 300, y: 300 }}
+          onDrag={ ::this.onDrag }
+        >
+          <div style={{
+            width: '20px',
+            height: '20px',
+            background: '#333',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+          />
         </Draggable>
       </div>
     );
