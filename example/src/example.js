9d6f55de5666409eb9666e0fdac6b7cf7bf34a85
bokuweb <bokuweb12@gmail.com>
2016-02-17 00:13:27 +0900

    Fix example

--- example/src/example.js
+++ example/src/example.js
@@ -5,7 +5,7 @@ export default class Example extends Component {
   render() {
     return (
       <Balloon
-         marker={ <i className="fa fa-github" style={{ fontSize: '48px' }}/> }
+         marker={ <i className="fa fa-comment" style={{ fontSize: '18px' }}/> }
       />
     );
   }
