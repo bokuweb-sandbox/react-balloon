e2e0c332a2d1e643c522ca858ea480e4b49080a8
bokuweb <bokuweb12@gmail.com>
2016-02-16 18:11:58 +0900

    Add github icon to example marker

--- example/src/example.js
+++ example/src/example.js
@@ -4,7 +4,9 @@ import Balloon from '../../src';
 export default class Example extends Component {
   render() {
     return (
-      <Balloon />
+      <Balloon
+         marker={ <i className="fa fa-github" style={{ fontSize: '48px' }}/> }
+      />
     );
   }
 }
